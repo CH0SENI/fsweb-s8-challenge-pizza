6c6a6434ece00a8a13a93c40b4f07930b3462647
CH0SENI <serdarfirattt@gmail.com>
2025-09-22 18:50:16 +0300

    DoughSelector.jsx ve OrderNote.jsx

--- src/components/DoughSelector.jsx
+++ src/components/DoughSelector.jsx
@@ -1,3 +1,16 @@
 import React from 'react';
 
-const DoughSelector = ({ selected, onChange })
+const DoughSelector = ({ selected, onChange }) => {
+    return (
+        <div className="form-group">
+      <label>Hamur Seç *</label>
+      <select value={selected} onChange={e => onChange(e.target.value)}>
+        <option value="kalın">Hamur Kalınlığı</option>
+        <option value="ince">İnce Hamur</option>
+        <option value="normal">Normal</option>
+      </select>
+    </div>
+    );
+}
+
+export default DoughSelector;
